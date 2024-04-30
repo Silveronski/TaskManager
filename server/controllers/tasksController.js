0f953e28166d35c415761c48e498465c5c8ac583
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-05-01 02:00:25 +0300

    changes

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Task = require('../models/taskModel');
+const {createCustomError} = require('../errors/customError');
 
 const getAllTasks = asyncHandler(async (req,res) => {
     const tasks = await Task.find({});
@@ -7,17 +8,19 @@ const getAllTasks = asyncHandler(async (req,res) => {
     res.status(200).json({tasks});
 });
 
-const createTask = asyncHandler(async (req,res) => {
+const createTask = asyncHandler(async (req,res,next) => {
     const {name} = req.body;
     if (!name) throw new Error('no name error');
     const task = await Task.create(req.body);
     res.status(201).json({task});
 });
 
-const getTask = asyncHandler(async (req,res) => {
+const getTask = asyncHandler(async (req,res,next) => {
     const task = await Task.findOne({_id: req.params.id});
-    if (!task) throw new Error('no task error');
-    res.status(200).json({task});
+    if (!task) {
+       return next(createCustomError('no task found', 404));
+    }
+    res.status(200).json({task});   
 });
 
 const updateTask = asyncHandler(async(req,res) => {
@@ -25,13 +28,17 @@ const updateTask = asyncHandler(async(req,res) => {
         new: true, // to return the updated task
         runValidators: true // to enfore model validators
     });
-    if (!task) throw new Error('no task to update error');
+    if (!task) {
+        return next(createCustomError('no task found', 404));
+    }
     res.status(200).json({task});
 });
 
 const deleteTask = asyncHandler(async (req,res) => {
     const task = await Task.findOneAndDelete({_id: req.params.id});
-    if (!task) throw new Error("no task to delete error");
+    if (!task) {
+        return next(createCustomError('no task found', 404));
+    }
     res.status(200).json({task});
 });
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const tasks = require('./routes/tasksRoutes');
 const connectToDb = require('./config/dbConnection');
+const notFound = require('./middleware/notFound');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
 connectToDb();
 const app = express();
@@ -9,6 +11,8 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
+app.use(errorHandlerMiddleware);
+app.use(notFound);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
